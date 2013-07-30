1f6ac7cac7f75af3575f1d25bd359ef238576e00
Yike Lu <ylu@keplercheuvreux.com>
2013-07-30 11:03:19 -0400

    "with" syntax works this way

--- timeout_ex.py
+++ timeout_ex.py
@@ -8,21 +8,23 @@ def pollingLoop():
     while True:
         #pudb.set_trace()
         job = gevent.spawn(myfunc, i)
-        try:
-            job.join(timeout=3)
-        except Timeout as e:
-            print "Timed out %s" % e
+        job.join()
         i = (i + 1) % 3
         gevent.sleep(1)
 
 def myfunc(i):
     #pudb.set_trace()
-    gevent.sleep(4)
-    d = {0: "HERE I AM",
-        1: "ROCK YOU LIKE A HURRICANE",
-        2: "YAAAA"}
-    print d[i]
+    try:
+        with Timeout(3):
+            gevent.sleep(4)
+            d = {0: "HERE I AM",
+                1: "ROCK YOU LIKE A HURRICANE",
+                2: "YAAAA"}
+            print d[i]
+    except Timeout as e:
+        print "Timed out: %s" % e
 
 if __name__ == "__main__":
     loop_job = gevent.spawn(pollingLoop)
-    gevent.joinall([loop_job])
+    loop_job.join()
+    #gevent.joinall([loop_job])
