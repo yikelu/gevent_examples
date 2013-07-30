7e6d8af0712ffdc88c7af22c706b333a298fa613
Yike Lu <ylu@keplercheuvreux.com>
2013-07-30 10:48:27 -0400

    This one doesn't work. Yeargh.

--- timeout_ex.py
+++ timeout_ex.py
@@ -6,24 +6,22 @@ import pudb
 def pollingLoop():
     i = 0
     while True:
+        #pudb.set_trace()
         job = gevent.spawn(myfunc, i)
-        job.join()
+        try:
+            job.join(timeout=3)
+        except Timeout as e:
+            print "Timed out %s" % e
         i = (i + 1) % 3
         gevent.sleep(1)
 
 def myfunc(i):
-    timeout = Timeout(3)
-    timeout.start()
-    try:
-        gevent.sleep(4)
-        d = {0: "HERE I AM",
-            1: "ROCK YOU LIKE A HURRICANE",
-            2: "YAAAA"}
-        print d[i]
-    except Timeout as e:
-        print 'Timeout caught: %s' % e
-    finally:
-        timeout.cancel()
+    #pudb.set_trace()
+    gevent.sleep(4)
+    d = {0: "HERE I AM",
+        1: "ROCK YOU LIKE A HURRICANE",
+        2: "YAAAA"}
+    print d[i]
 
 if __name__ == "__main__":
     loop_job = gevent.spawn(pollingLoop)
